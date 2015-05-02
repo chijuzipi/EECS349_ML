ce65c82aaf537c2f3f6a0a8a611b0ab8300a58cd
chijuzipi <yuzhoubrother@gmail.com>
2015-05-01 19:15:59 -0500

    push

--- homework2/src/learner.py
+++ homework2/src/learner.py
@@ -23,20 +23,25 @@ class Learner():
   def __init__(self): 
     
     if len(sys.argv) <= 1:
-      #train_file = "../data/train_test3.csv"
-      train_file = "../data/origin/btrain.csv"
+      #train_file = "../data/origin/btrain.csv"
+      train_file = "../data/test.csv"
     else:
       train_file = sys.argv[1]
 
     f = open(train_file, "r")
     lines = f.readlines()
+    lines = lines[:5000]
     
     # get the list of attributes
-    attrNames, records, resultAttr = readData(lines)
+    attrNames, records, resultAttr, typeList = readData(lines)
+    print records
+    return 
+    process(records, attrNames, typeList)
+
     print "read " +  str(len(attrNames)) + " attributes"
     print "read " +  str(len(records))   + " records"
 
-    printTree(self.DTL(records, attrNames, resultAttr, 1), -1)
+    printTree(self.DTL(records, attrNames, resultAttr), -1)
 
   def DTL(self, records, attrNames, resultAttr):
     resultValues  = [record[resultAttr] for record in records]
@@ -44,7 +49,7 @@ class Learner():
     
     # means follow the path, every thing is the same, but the result may still be different
     # therefore return majority
-    if len(records) == 0 or len(attrNames) == 0:
+    if len(records) == 0 or len(attrNames) == 1:
       return Node(None, default)
 
     elif resultValues.count(resultValues[0]) == len(resultValues):
@@ -52,6 +57,7 @@ class Learner():
       
     else:
       bestAttr = getBestAttr(records, attrNames, resultAttr)
+      print bestAttr
       #print "the chosen attribute is : " + str(bestAttr)
       # construct a 2-dimeinsal array that stores value for bestAttr, and result
       attrResultList = getTwoDimensionArray(records, bestAttr, resultAttr)
@@ -60,6 +66,7 @@ class Learner():
       newList = sorted(attrResultList, key=lambda x: x[0], reverse=False)
       newDictList = []
       for item in newList:
+       
         record = {} 
         record[resultAttr] = item[1]
         newDictList.append(record)
@@ -70,20 +77,42 @@ class Learner():
       tree = Node(bestAttr, selectVal)
       
       # No need to remove the bestAttr from the attrNames, since the bestAttr may need to show up again
-      newAttrNames = getNewAttrNames(attrNames, bestAttr) 
+      #attrNames = getNewAttrNames(attrNames, bestAttr) 
       #print "the new attribute is " + str(newAttrNames)
 
       # add left and right child
       leftNewRecords, rightNewRecords = getNewRecords(records, bestAttr, selectVal)
-      #print len(leftNewRecords)
-      #print len(rightNewRecords)
+      print len(leftNewRecords)
+      print len(rightNewRecords)
 
-      leftChild  = self.DTL(leftNewRecords, newAttrNames, resultAttr, fitFunction)
-      rightChild = self.DTL(rightNewRecords, newAttrNames, resultAttr, fitFunction)
+      leftChild  = self.DTL(leftNewRecords, attrNames, resultAttr)
+      rightChild = self.DTL(rightNewRecords, attrNames, resultAttr)
       tree.left  = leftChild
       tree.right = rightChild
 
     return tree
+
+def process(records, attrNames, typeList):
+  # default table hold default value for any attribute, based on the type and records
+  defaultTable = {}
+  for i in range(len(attrNames)):
+    attr = attrNames[i]
+    typ  = typeList[i]
+    resultValues  = [record[attr] for record in records]
+    resultValues = [x for x in resultValues if x != ' ?' and x != '?']
+    defaultVal = "none"
+    if typ == "numeric":
+      defaultVal = numpy.mean(resultValues)
+    elif typ == "nominal":
+      defaultVal = getMajority(records, attr)
+    defaultTable[attr] = defaultVal
+
+  for i in range(len(attrNames)):
+    attr = attrNames[i]
+    typ  = typeList[i]
+    for record in records:
+      if record[attr] == '?':
+        record[attr] = defaultTable[attr]
 
 def getEntropy(records, resultAttr):
   entropy = 0.0
@@ -95,13 +124,16 @@ def getEntropy(records, resultAttr):
 def getSelectVal(newDictList, resultAttr):
   maxEntropy = 0.0 
   selectIndex = 0
-  for index in range(len(newDictList)):
-    leftEntropy  = getEntropy(newDictList[:index], resultAttr) * len(newDictList[:index])/len(newDictList)
-    rightEntropy = getEntropy(newDictList[index:], resultAttr) * len(newDictList[index:])/len(newDictList)
-    totalEntropy = leftEntropy + rightEntropy
-    if totalEntropy > maxEntropy:
-      selectIndex = index
-      maxEntropy = totalEntropy
+  #return randint(0, len(newDictList)-1)
+  for index in range(len(newDictList)-1):
+    # only calculate at the index where the result value changes
+    if newDictList[index][resultAttr] != newDictList[index+1][resultAttr]:
+      leftEntropy  = getEntropy(newDictList[:index], resultAttr) * len(newDictList[:index])/len(newDictList)
+      rightEntropy = getEntropy(newDictList[index:], resultAttr) * len(newDictList[index:])/len(newDictList)
+      totalEntropy = leftEntropy + rightEntropy
+      if totalEntropy > maxEntropy:
+        selectIndex = index
+        maxEntropy = totalEntropy
 
   return selectIndex
 
@@ -124,12 +156,12 @@ def printTree(tree, n):
   n += 1
   #print left child
   print n * "| ",
-  print tree.name + "<" + tree.value,
+  print tree.name + "<" + str(tree.value),
   printTree(tree.left, n)
 
   #print right child
   print n * "| ",
-  print tree.name + ">=" + tree.value,
+  print tree.name + ">=" + str(tree.value),
   printTree(tree.right, n)
 
 def getInfoGain(records, attr, resultAttr):
@@ -193,45 +225,52 @@ def getMajority(records, resultAttr):
 
 def getBestAttr(records, attrNames, resultAttr):
   # random choose attribute
+  
   output   = attrNames[0]
   maxGain = 0.0
   for attr in attrNames:
+    if attr == resultAttr:
+      continue
     infoGain = getInfoGain(records, attr, resultAttr)
     if infoGain > maxGain:
       output = attr
       maxGain = infoGain
-  if maxGain == 0.0:
-    return output, 0
-  else:
-    return output, 1
-
-  return ''
+  return output
     
 # generate the attribute list and example list
 def readData(lines):
   # construct the attrbute name list
-  dividList = lines[0].split(',')
   attrList = []
-
+  dividList = lines[0].split(',')
   for attr in dividList:
-    attr = re.compile(r'[\n\r\t]').sub(' ', attr).rstrip()
+    print attr
+    attr = re.compile(r'[\n\r\t]').sub(' ', attr).strip()
+    print "after process: " + attr
     attrList.append(attr)
     
-  recordList = []
-  
+  # construct the type list: noimal and numeric
+  typeList = []
+  dividList = lines[1].split(',')
+  for attr in dividList:
+    attr = re.compile(r'[\n\r\t]').sub(' ', attr).strip()
+    typeList.append(attr)
+
   #construct the record list for each attribute
-  for i in range(1, len(lines)):
+  recordList = []
+  for i in range(2, len(lines)):
     line = lines[i]
     dataList = line.split(',')
     record = {}
     for j in range(len(dataList)):
       data = dataList[j]
       data = re.compile(r'[\n\r\t]').sub(' ', data).rstrip()
+      if data != '?' and data != ' ?':
+        data = float(data) 
       attrName = attrList[j]
       record[attrName] = data
     recordList.append(record)
 
-  return attrList[:len(attrList)-1], recordList, attrList[len(attrList)-1]
+  return attrList, recordList, attrList[len(attrList)-1], typeList
   
 def main():
   learner = Learner()
